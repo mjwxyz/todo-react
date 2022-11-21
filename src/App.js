a175458f95228ffb5e1f1306c601cb55ffb3f0a0
mjwxyz <michaeljb305@gmail.com>
2022-11-21 07:12:34 -0500

    added TaskForm and Task component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import TaskForm from './TaskForm';
+import Task from './Task';
+
+function App() {
+  return (
+    <main>
+      <TaskForm />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Task.js
@@ -0,0 +1,10 @@
+import Checkbox from "./Checkbox"
+
+export default function Task() {
+    return (
+        <div className="task">
+            <Checkbox defaultChecked={true} />
+            test task
+        </div>
+    )
+}
